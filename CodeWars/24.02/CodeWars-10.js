116dff9a337dcdc8105649be5b5293623e107b32
lelka00777 <lelka00777@gmail.com>
2022-02-23 19:19:42 +0300

    CodeWars 24.02

---
+++ CodeWars/24.02/CodeWars-10.js
@@ -0,0 +1,10 @@
+		// Beginner - Lost Without a Map
+
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+
+function maps(x){
+let res = x.map((xItem) => {
+  return xItem * 2;
+});
+  return res;
+}
