04ba5522a1f67643ce170ab89932f60ec147f379
lelka00777 <lelka00777@gmail.com>
2022-02-23 19:10:31 +0300

    CodeWars 24.02

---
+++ CodeWars/24.02/CodeWars-1.js
@@ -0,0 +1,7 @@
+//Regular Ball Super Ball
+
+https://www.codewars.com/kata/53f0f358b9cb376eca001079/train/javascript
+
+var Ball = function(ballType) {
+   ballType === undefined ? this.ballType = 'regular' : this.ballType = 'super'
+};
