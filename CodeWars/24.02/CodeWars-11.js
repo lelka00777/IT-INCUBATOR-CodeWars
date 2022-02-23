bb4273072a1c679c9bf269578c2634394398ee00
lelka00777 <lelka00777@gmail.com>
2022-02-23 19:20:59 +0300

    CodeWars 24.02

---
+++ CodeWars/24.02/CodeWars-11.js
@@ -0,0 +1,11 @@
+          //	 Is he gonna survive?
+
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+
+function hero(bullets, dragons){
+return bullets / dragons >= 2;
+}
+
+
+
